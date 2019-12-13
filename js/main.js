959906fad8e068d1469f185777e6ef16ee180435
Michał Skoczek <mwskoczek@gmailcom>
2019-12-13 21:06:37 +0100

    add arrow function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 const hamburger = document.querySelector('.header__button-menu');
 const nav = document.querySelector('.navigation');
 
+const arrow = document.querySelector('.arrow');
+
+
 const handleClick = () => {
   hamburger.classList.toggle('header__button-menu--active');
   nav.classList.toggle('navigation--active');
 }
 
+const moveUp = () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  })
+}
+
 hamburger.addEventListener('click', handleClick);
+arrow.addEventListener('click', moveUp);
